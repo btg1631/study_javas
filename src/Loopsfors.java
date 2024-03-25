4f623b8d30cb8512db115cacb4eda71cf72ab9e5
youngji Jang <btg1631@naver.com>
2024-03-25 08:36:59 +0000

    add pollsWithoutDB

--- src/Loopsfors.java
+++ src/Loopsfors.java
@@ -1,6 +1,31 @@
 // refer : https://www.w3schools.com/java/java_for_loop.asp
+
+import java.util.ArrayList;
+
 public class Loopsfors {
     public static void main(String[] args){
+        //  <> 캐스팅? 캐스트?
+        ArrayList<String> listFirst = new ArrayList<String>();
+        // 값 넣기
+        listFirst.add("Volvo");
+        listFirst.add("BMW");
+        listFirst.add("Ford");
+
+        // for (statement1;....) {
+        //     ...
+        // }
+        for (int i=0; i<listFirst.size(); i++){
+            System.out.println("for count "+listFirst.get(i));
+        }
+
+        // for (type variableName : arrayName) {
+        //     code block to be executed
+        // }
+        for (String str:listFirst){
+            System.out.println("for each "+str);
+        }
+
+        System.out.println("Hello World");
         /* 
             for 동작 위한 조건 : 시작점, 종료 조건, 반복 횟수
             statement 1 : 시작점
---
+++ src/quests/pollsWithoutDB.java
@@ -0,0 +1,36 @@
+package quests;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class pollsWithoutDB {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        ArrayList<String> listquestion = new ArrayList<String>();
+        listquestion.add("인터스텔라 선호도");
+        listquestion.add("해리포터 선호도");
+        listquestion.add("파묘 선호도");
+
+        ArrayList<String> listanswer = new ArrayList<String>();
+        listanswer.add("1) 좋음");
+        listanswer.add("2) 중간");
+        listanswer.add("3) 나쁨");
+
+        ArrayList<String> listresult = new ArrayList<String>();
+
+        for (int i=0; i<listquestion.size(); i++){
+            System.out.println(i+1+". "+listquestion.get(i));
+            for (int j=0; j<listanswer.size(); j++){
+                System.out.println(listanswer.get(j));
+            }
+
+            // 답한 내용 받기
+            System.out.print("답하기 : ");
+            String result = scanner.nextLine();
+            listresult.add(result);
+        }
+        System.out.println("답한 내용 출력: "+listresult);
+        scanner.close();
+    }
+}   
