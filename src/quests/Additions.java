b31e2c94b827a9e4c96ea73367e5dca1bcdba59f
youngji Jang <btg1631@naver.com>
2024-03-25 05:39:38 +0000

    add Additions

--- src/quests/Additions.java
+++ src/quests/Additions.java
@@ -1,5 +1,20 @@
 package quests;
 
+import java.util.Scanner;
+
 public class Additions {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("intFirst : ");
+        int intFirst = scanner.nextInt();
+
+        System.out.print("intSecond : ");
+        int intSecond = scanner.nextInt();
+
+        int result = intFirst + intSecond;
+        System.out.println(intFirst+" + "+intSecond+" = "+result);
+
+        scanner.close();  // 자원 반납
+    }
 }
