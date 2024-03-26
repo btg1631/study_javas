fbcddaa071f62d0b502244a4911b0af4e7656cda
youngji Jang <btg1631@naver.com>
2024-03-26 08:07:11 +0000

    push

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # study_java
 
+## ☑ 사용기술
+<img alt="이미지명" src ="https://img.shields.io/badge/PYTHON-3776AB.svg?&style=for-the-badge&logo=PYTHON&logoColor=white"/> <img alt="이미지명" src ="https://img.shields.io/badge/visual studio code-007ACC.svg?&style=for-the-badge&logo=visualstudiocode&logoColor=white"/> <img alt="이미지명"
+
+## 💻 java
+|*|제목|code|설명|비고|
+|--|--|--|--|--|
+
+
+
+# 📈 quests
+
--- src/TypeCastIntegers.java
+++ src/TypeCastIntegers.java
@@ -1,5 +1,25 @@
+// https://www.w3schools.com/java/java_type_casting.asp
+
+import java.util.Scanner;
+
 public class TypeCastIntegers {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        // 두개 값 받기
+        String strFirst = scanner.nextLine();
+        String strSecond = scanner.nextLine();
+
+        // 값을 받고싶을 때 Integer 사용?? 연산할 때는 캐스트 사용??
+        int intFirst = Integer.parseInt(strFirst);
+        int intSecond = Integer.parseInt(strSecond);
+
+        // strFirst + strSecond
+        // "12"
+        // intFirst + intSecond
+        // 3
+        // (int) strFirst
+
+        scanner.close();
     }
 }
